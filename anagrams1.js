c2727e484e294646437e14346217774260e8613b
gabrielle@Gabbys-MacPro.com <gab_morrow@yahoo.com>
2020-01-10 11:42:18 -0500

    push

---
+++ anagrams1.js
@@ -0,0 +1,31 @@
+const button = document.getElementById("findButton");
+const div = document.getElementById("output")
+button.onclick = function() {
+  div.innerHTML = ""
+  let typedText = document.getElementById("input").value;
+  let anagrams = getAnagramsOf(typedText);
+  let element = document.createElement("h3")
+  element.textContent = anagrams
+  div.appendChild(element)
+  
+}
+function getAnagramsOf(text) {
+  let order = alphabetize(text);
+  let newArray = [];
+  for (i = 0; (i < words.length); i++) {
+    if (order === alphabetize(words[i])) {
+      newArray.push(words[i])
+    }
+  }
+  return (newArray)
+}
+
+function alphabetize(a) {
+  
+  return a.toLowerCase()
+    .split("")
+    .sort()
+    .join("")
+    .trim();
+}
+
